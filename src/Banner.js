7a365717c5e16e302644c6dbaddd3259a58b041e
fastlain <michaeldallain@gmail.com>
2018-10-01 10:48:35 -0700

    Add Banner

---
+++ src/Banner.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import './banner.css'
+
+class Banner extends Component {
+    render() {
+      return (
+        <section>
+            <h1>Michael Allain</h1>
+            <div className='separator'></div>
+            <h2>FULL STACK WEB DEVELOPER</h2>
+        </section>
+      );
+    }
+  }
+  
+  export default Banner;
+  
