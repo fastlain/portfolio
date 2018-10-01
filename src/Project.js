223db2491fdafc322c913d7776826e183757ff10
MichaelAllain <michaeldallain@gmail.com>
2018-10-01 15:07:28 -0700

    Center project title

--- src/Project.js
+++ src/Project.js
@@ -5,7 +5,7 @@ class Project extends Component {
     render() {
         return (
             <div>
-                <h3>Project Title</h3>
+                <h3 className="projTitle">Project Title</h3>
                 <p>Project description...Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
                 <div className="projBtnCont">
                     <button className="projBtn" type="button">
