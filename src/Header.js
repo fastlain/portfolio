4f945044a5ebb4a0997ece8c527cae578a6338c1
MichaelAllain <michaeldallain@gmail.com>
2018-10-01 10:39:31 -0700

    Add Header

--- src/Header.js
+++ src/Header.js
@@ -1,13 +1,16 @@
 import React, { Component } from 'react';
+import './header.css';
 
 class Header extends Component {
   render() {
     return (
-      <div >
-        <header>
-          <h1>Michael's Portfolio</h1>
-        </header>
-      </div>
+      <header className='pageHeader'>
+        <nav className='navFlex'>
+          <a href='#' className='navLink'>ABOUT</a>
+          <a href='#' className='navLink'>PROJECTS</a>
+          <a href='#' className='navLink'>CONTACT</a>
+        </nav>
+      </header>
     );
   }
 }
