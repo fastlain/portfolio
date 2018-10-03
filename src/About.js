220f8ff67f9fdfe6de98e080a188f6e70295c996
fastlain <michaeldallain@gmail.com>
2018-10-03 10:06:56 -0700

    Add About data, update layout

--- src/About.js
+++ src/About.js
@@ -1,12 +1,21 @@
 import React, {Component} from 'react';
 import styles from './About.module.css';
+import data from './data/aboutdata';
 
 class About extends Component {
     render() {
+
+        let paragraphs = data.description.map((para, i) => <p key={i} className={styles.para}>{para}</p>);
+
         return (
             <section id="about">
                 <h2>ABOUT</h2>
-                <p className={styles.para}>About me...Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                <div className={styles.flexRow}>
+                    <img className={styles.headshot} src={data.headshot} alt="Michael Allain headshot"/>
+                    <div className={styles.flexCol}>
+                        {paragraphs}
+                    </div>
+                </div>
             </section>
         );
     }
---
+++ src/data/aboutdata.js
@@ -0,0 +1,13 @@
+// This file contains project information to be imported by React components 
+
+const aboutdata = {
+    description: [
+        "Hi, I'm a Full Stack Developer based in Portland, OR",
+        "I'm rapidly learning exciting web technologies through Thinkful’s Full Stack bootcamp, focusing on the MERN stack (Mongoose, Express, React, Node). I love bringing great ideas to life by building web apps that are both useful and fun. As a medical doctor and researcher in a former life, I've learned how to master new skills quickly, communicate effectively with people of diverse backgrounds, and manage large amounts of complex information.",
+        "When I'm not in front of a keyboard, you'll find me enjoying stereotypical PNW activities like hiking, camping, fishing, gardening, beer tasting, and hunting wild mushrooms.",
+        "I am currently seeking a junior developer position in the Portland area. I'm eager both to contribute my existing skills and to learn new tech on the job!"
+    ],
+    headshot: "images/headshot.jpg"
+}
+
+export default aboutdata;
