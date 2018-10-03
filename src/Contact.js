3a8f5d858c7e9083af2373629b064fc6c5f9e53b
fastlain <michaeldallain@gmail.com>
2018-10-03 11:27:42 -0700

    Add contact data, icons, layout

--- src/Contact.js
+++ src/Contact.js
@@ -1,20 +1,38 @@
 import React, {Component} from 'react';
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import {faEnvelope} from '@fortawesome/free-solid-svg-icons';
+import {faLinkedin} from '@fortawesome/free-brands-svg-icons';
+import {faGithubSquare} from '@fortawesome/free-brands-svg-icons';
+
+
 import styles from './Contact.module.css';
+import data from './data/contactdata';
 
 class Contact extends Component {
     render() {
         return (
-            <section id="contact">
+            <section id="contact" className={styles.contactSection}>
                 <h2>CONTACT</h2>
-                <div className={styles.contact}>
-                    <a href="mailto:michaeldallain@gmail.com" target="_blank" rel="noopener noreferrer">Email</a>                
-                </div>
-                <div className={styles.contact}>
-                    <a href="https://www.linkedin.com/in/allainm/" target="_blank" rel="noopener noreferrer">LinkedIn</a>                
-                </div>
-                <div className={styles.contact}>
-                    <a href="https://github.com/fastlain" target="_blank" rel="noopener noreferrer">GitHub</a>                
-                </div>
+                <ul className={styles.contactList}>
+                    <li className={styles.contact}>
+                        <a className={styles.link} href={`mailto:${data.email}?subject=Reaching%20out%20from%20your%20portfolio`} target="_blank" rel="noopener noreferrer">
+                            <FontAwesomeIcon icon={faEnvelope} className={styles.icon}/>
+                            Email
+                        </a>                
+                    </li>
+                    <li className={styles.contact}>
+                        <a className={styles.link} href={data.linkedInLink} target="_blank" rel="noopener noreferrer">
+                            <FontAwesomeIcon icon={faLinkedin} className={styles.icon}/>
+                            LinkedIn
+                        </a>                
+                    </li>
+                    <li className={styles.contact}>
+                        <a className={styles.link} href={data.gitHubLink}target="_blank" rel="noopener noreferrer">
+                            <FontAwesomeIcon icon={faGithubSquare} className={styles.icon}/>
+                            GitHub
+                        </a>                
+                    </li>
+                </ul>
             </section>
         );
     }
---
+++ src/data/contactdata.js
@@ -0,0 +1,9 @@
+// This file contains contact information to be imported by React components 
+
+const contactdata = {
+    email: "michaeldallain@gmail.com",
+    linkedInLink: "https://www.linkedin.com/in/allainm/",
+    gitHubLink: "https://github.com/fastlain"
+}
+
+export default contactdata;
