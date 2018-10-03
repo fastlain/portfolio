b63957dfe98e25403c9cbc50ed8ab316608756e6
fastlain <michaeldallain@gmail.com>
2018-10-03 16:29:26 -0700

    Add flexbox to Projects

--- src/Project.js
+++ src/Project.js
@@ -8,17 +8,25 @@ class Project extends Component {
 
     render() {
         return (
-            <div>
+            <div className={styles.project}>
                 <h3 className={styles.title}>{this.props.title}</h3>
-                <img className={styles.screenshot} src={this.props.screenshot} alt={this.props.title + " Screenshot"} />            
-                <p className={styles.para}>{this.props.description}</p>
-                <div className={styles.btnCont}>
-                    <button className={styles.btn} type="button">
-                        <a href={this.props.liveLink} target="_blank" rel="noopener noreferrer">Live Demo</a>
-                    </button>
-                    <button className={styles.btn} type="button">
-                        <a href={this.props.gitHubLink} target="_blank" rel="noopener noreferrer">GitHub Repo</a>
-                    </button>
+                <div className={styles.flexRow}>
+                    <div className={styles.imgContainer}>
+                        <img className={styles.screenshot} src={this.props.screenshot} alt={this.props.title + " Screenshot"} />            
+                    </div>
+                    <div className={styles.flexCol}>
+                        <p className={styles.para}>
+                            {this.props.description}
+                        </p>
+                        <div className={styles.btnCont}>
+                            <button className={styles.btn} type="button">
+                                <a href={this.props.liveLink} target="_blank" rel="noopener noreferrer">Live Demo</a>
+                            </button>
+                            <button className={styles.btn} type="button">
+                                <a href={this.props.gitHubLink} target="_blank" rel="noopener noreferrer">GitHub Repo</a>
+                            </button>
+                        </div>
+                    </div>
                 </div>
             </div>
         );
