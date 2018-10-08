14bf6e05efcfb6ee99e07140856eb9fbe5f5f7ca
fastlain <michaeldallain@gmail.com>
2018-10-08 16:27:12 -0700

    Refactor Contact component and create ContactLink
    The individual links in the contact section were put into their own component.
    This allowed for handling on hover state for individual links and simplified
    CSS styling and animation. Animation effects were added to underline links on hove

--- src/Contact.js
+++ src/Contact.js
@@ -1,37 +1,23 @@
 import React, {Component} from 'react';
-import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import ContactLink from './ContactLink'
+
 import {faEnvelope} from '@fortawesome/free-solid-svg-icons';
 import {faLinkedin} from '@fortawesome/free-brands-svg-icons';
 import {faGithubSquare} from '@fortawesome/free-brands-svg-icons';
-
 
 import styles from './Contact.module.css';
 import data from './data/contactdata';
 
 class Contact extends Component {
+    
     render() {
         return (
             <section id="contact" className={styles.contactSection}>
                 <h2>CONTACT</h2>
                 <ul className={styles.contactList}>
-                    <li className={styles.contact}>
-                        <a className={styles.link} href={`mailto:${data.email}?subject=Reaching%20out%20from%20your%20portfolio`} target="_blank" rel="noopener noreferrer">
-                            <FontAwesomeIcon icon={faEnvelope} fixedWidth className={styles.icon}/>
-                            Email
-                        </a>                
-                    </li>
-                    <li className={styles.contact}>
-                        <a className={styles.link} href={data.linkedInLink} target="_blank" rel="noopener noreferrer">
-                            <FontAwesomeIcon icon={faLinkedin} fixedWidth className={styles.icon}/>
-                            LinkedIn
-                        </a>                
-                    </li>
-                    <li className={styles.contact}>
-                        <a className={styles.link} href={data.gitHubLink}target="_blank" rel="noopener noreferrer">
-                            <FontAwesomeIcon icon={faGithubSquare} fixedWidth className={styles.icon}/>
-                            GitHub
-                        </a>                
-                    </li>
+                    <ContactLink linkTarget={`mailto:${data.email}?subject=Reaching%20out%20from%20your%20portfolio`} icon={faEnvelope} label="Email" />
+                    <ContactLink linkTarget={data.linkedInLink} icon={faLinkedin} label="LinkedIn" />  
+                    <ContactLink linkTarget={data.gitHubLink} icon={faGithubSquare} label="GitHub" />           
                 </ul>
             </section>
         );
---
+++ src/ContactLink.js
@@ -0,0 +1,34 @@
+import React, {Component} from 'react';
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import styles from './ContactLink.module.css';
+
+class ContactLink extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            isHovered: false
+        }
+    }
+
+    handleHover = () => {
+        this.setState({
+            isHovered: !this.state.isHovered
+        });
+    }
+    
+    render() {
+        // set CSS class for link based on hover state
+        const hoverClass = this.state.isHovered ? `${styles.linkText} ${styles.hoveredLinkText}` : styles.linkText;
+
+        return (
+            <li className={styles.contact} onMouseEnter={this.handleHover} onMouseLeave={this.handleHover}>
+                <a className={styles.link} href={this.props.linkTarget} target="_blank" rel="noopener noreferrer">
+                    <FontAwesomeIcon icon={this.props.icon} fixedWidth className={styles.icon}/>
+                    <span className={hoverClass}>{this.props.label}</span>
+                </a>                
+            </li>
+        )
+    }
+}
+
+export default ContactLink;
