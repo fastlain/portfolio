33b60f0801ccce550bf9d3ffbed61295dfc71fa2
fastlain <michaeldallain@gmail.com>
2018-10-08 13:54:46 -0700

    Add animaton to Banner

--- src/Banner.js
+++ src/Banner.js
@@ -1,13 +1,19 @@
 import React, { Component } from 'react';
 import styles from './Banner.module.css';
+import Fade from 'react-reveal/Fade';
 
 class Banner extends Component {
     render() {
       return (
         <section className={styles.banner}>
-            <h1 className={styles.mainHeading}>Michael Allain</h1>
-            <div className={styles.separator}></div>
-            <h2 className={styles.subHeading}>FULL STACK WEB DEVELOPER</h2>
+            <Fade bottom duration='1500'>
+              <h1 className={styles.mainHeading}>Michael Allain</h1>
+            </Fade>
+            
+            <Fade duration='2000' delay='1500'>
+              <div className={styles.separator}></div>
+              <h2 className={styles.subHeading}>FULL STACK WEB DEVELOPER</h2>
+            </Fade>
         </section>
       );
     }
