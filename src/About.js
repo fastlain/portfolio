45f795d99adc35e34dd35ca59fb73bcd09755e66
fastlain <michaeldallain@gmail.com>
2018-10-08 14:07:35 -0700

    Add animation to About

--- src/About.js
+++ src/About.js
@@ -1,6 +1,9 @@
 import React, {Component} from 'react';
 import styles from './About.module.css';
 import data from './data/aboutdata';
+import Fade from 'react-reveal/Fade';
+import RubberBand from 'react-reveal/RubberBand';
+
 
 class About extends Component {
     render() {
@@ -10,15 +13,19 @@ class About extends Component {
         return (
             <section id="about">
                 <h2>ABOUT</h2>
-                <div className={styles.flexRow}>
-                    <div className={styles.imgContainer}>    
-                        <img className={styles.headshot} src={data.headshot} alt="Michael Allain headshot"/>
-                        <p className={styles.tagLine}>{data.tagline}</p>
+                <Fade right>
+                    <div className={styles.flexRow}>
+                        <div className={styles.imgContainer}>    
+                            <RubberBand delay='1000'>
+                                <img className={styles.headshot} src={data.headshot} alt="Michael Allain headshot"/>
+                            </RubberBand>
+                            <p className={styles.tagLine}>{data.tagline}</p>
+                        </div>
+                        <div className={styles.flexCol}>
+                            {paragraphs}
+                        </div>
                     </div>
-                    <div className={styles.flexCol}>
-                        {paragraphs}
-                    </div>
-                </div>
+                </Fade>
             </section>
         );
     }
