afa92cc06e1be428596c4161c846be8578115702
fastlain <michaeldallain@gmail.com>
2018-10-08 16:44:45 -0700

    Add animation to Contacts

--- src/Contact.js
+++ src/Contact.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import ContactLink from './ContactLink'
+import Bounce from 'react-reveal/Bounce';
 
 import {faEnvelope} from '@fortawesome/free-solid-svg-icons';
 import {faLinkedin} from '@fortawesome/free-brands-svg-icons';
@@ -13,12 +14,19 @@ class Contact extends Component {
     render() {
         return (
             <section id="contact" className={styles.contactSection}>
-                <h2>CONTACT</h2>
-                <ul className={styles.contactList}>
-                    <ContactLink linkTarget={`mailto:${data.email}?subject=Reaching%20out%20from%20your%20portfolio`} icon={faEnvelope} label="Email" />
-                    <ContactLink linkTarget={data.linkedInLink} icon={faLinkedin} label="LinkedIn" />  
-                    <ContactLink linkTarget={data.gitHubLink} icon={faGithubSquare} label="GitHub" />           
-                </ul>
+                    <h2>CONTACT</h2>
+                    <ul className={styles.contactList}>
+                        <Bounce left>
+                            <ContactLink linkTarget={`mailto:${data.email}?subject=Reaching%20out%20from%20your%20portfolio`} icon={faEnvelope} label="Email" />
+                        </Bounce>
+                        <Bounce left delay={100}>
+                            <ContactLink linkTarget={data.linkedInLink} icon={faLinkedin} label="LinkedIn" />  
+                        </Bounce>
+                        <Bounce left delay={200}>
+                            <ContactLink linkTarget={data.gitHubLink} icon={faGithubSquare} label="GitHub" />           
+                        </Bounce>
+                    </ul>
+
             </section>
         );
     }
