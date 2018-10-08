6c6e399469e6e41ab6e983086b7eb6558a1c0d88
fastlain <michaeldallain@gmail.com>
2018-10-08 13:48:20 -0700

    Add Bounce animation to Projects

--- src/ProjectsContainer.js
+++ src/ProjectsContainer.js
@@ -1,16 +1,22 @@
 import React, {Component} from 'react';
 import Project from './Project';
 import data from './data/projectdata';
+import Bounce from 'react-reveal/Bounce';
 
 class ProjectsContainer extends Component {
     render() {
 
-        let projects = data.map((proj, i) =>
-            <Project key={i} order={i%2} {...proj}/>
-        )
+        let projects = data.map((proj, i) => {
+            let bounceside = i%2 === 0;
+            return (
+                <Bounce left={bounceside} right={!bounceside}>
+                    <Project key={i} order={i%2} {...proj}/>
+                </Bounce>
+            );
+        });
 
         return (
-            <section id="projects">
+            <section id="projects" style={{overflow: 'hidden'}}>
                 <h2>PROJECTS</h2>
                 {projects}
             </section>
