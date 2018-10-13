9e70ae42e12e062cc328dd7dd0093df88c16fe8b
fastlain <michaeldallain@gmail.com>
2018-10-12 17:50:37 -0700

    Convert Technology to stateless component

--- src/Technology.js
+++ src/Technology.js
@@ -1,37 +1,33 @@
-import React, {Component} from 'react';
+import React from 'react';
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
 import {faCode} from '@fortawesome/free-solid-svg-icons';
 import styles from './Technology.module.css';
 
-class Technology extends Component {
-    render() {
-        
-        let useHeadings = this.props.tech.length > 1;
-        let techdata = this.props.tech.map(techGroup => {
-            return (
-                <div key={techGroup.category}>
-                    {useHeadings ? <h5 className={styles.catHeading}>{techGroup.category}</h5> : null}
-                    <ul className={styles.list}>
-                        {techGroup.techs.map(tech => <li className={styles.techItem} key={tech}>{tech}</li>)}
-                    </ul>
-                </div>
-            );
-        });
-
+const Technology = ({tech}) => {
+    let useHeadings = tech.length > 1;
+    let techdata = tech.map(techGroup => {
         return (
-            <div>
-
-                <h4 className={styles.techHeading}>
-                    <FontAwesomeIcon icon={faCode} className={styles.icon}/>
-                    Technology
-                    <FontAwesomeIcon icon={faCode} className={styles.icon}/>
-                </h4>
-                <div className={styles.container}>
-                    {techdata}
-                </div>
+            <div key={techGroup.category}>
+                {useHeadings ? <h5 className={styles.catHeading}>{techGroup.category}</h5> : null}
+                <ul className={styles.list}>
+                    {techGroup.techs.map(tech => <li className={styles.techItem} key={tech}>{tech}</li>)}
+                </ul>
             </div>
         );
-    }
+    });
+
+    return (
+        <div>
+            <h4 className={styles.techHeading}>
+                <FontAwesomeIcon icon={faCode} className={styles.icon}/>
+                    Technology
+                <FontAwesomeIcon icon={faCode} className={styles.icon}/>
+            </h4>
+            <div className={styles.container}>
+                {techdata}
+            </div>
+        </div>
+    );
 }
 
 export default Technology;
