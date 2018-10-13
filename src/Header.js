40fbf6b7e7fa521105c5083eaecf0e4d5d82cef7
fastlain <michaeldallain@gmail.com>
2018-10-12 17:57:42 -0700

    Convert Header to stateless component

--- src/Header.js
+++ src/Header.js
@@ -1,25 +1,23 @@
-import React, { Component } from 'react';
+import React from 'react';
 import styles from './Header.module.css';
 import { Link } from 'react-scroll'
 
-class Header extends Component {
-	render() {
-		return (
-			<header className={styles.pageHeader}>
-				<nav className={styles.flex}>
-					<Link to="scrollAbout" smooth={true} duration={500} offset={-25}>
-						<span className={styles.navLink}>ABOUT</span>
-					</Link>
-					<Link to="scrollProjects" smooth={true} duration={500} offset={-25}>
-						<span className={styles.navLink}>PROJECTS</span>
-					</Link>
-					<Link to="scrollContact" smooth={true} duration={500}>
-						<span className={styles.navLink}>CONTACT</span>
-					</Link>
-				</nav>
-			</header>
-		);
-	}
+const Header = () => {
+	return (
+		<header className={styles.pageHeader}>
+			<nav className={styles.flex}>
+				<Link to="scrollAbout" smooth={true} duration={500} offset={-25}>
+					<span className={styles.navLink}>ABOUT</span>
+				</Link>
+				<Link to="scrollProjects" smooth={true} duration={500} offset={-25}>
+					<span className={styles.navLink}>PROJECTS</span>
+				</Link>
+				<Link to="scrollContact" smooth={true} duration={500}>
+					<span className={styles.navLink}>CONTACT</span>
+				</Link>
+			</nav>
+		</header>
+	);
 }
 
 export default Header;
