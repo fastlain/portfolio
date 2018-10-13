fc3ba0fa3b9edd8c55473d9b69711659f2d8d8cc
fastlain <michaeldallain@gmail.com>
2018-10-12 18:02:36 -0700

    Convert About to stateless component

--- src/About.js
+++ src/About.js
@@ -1,34 +1,29 @@
-import React, {Component} from 'react';
+import React from 'react';
 import styles from './About.module.css';
 import data from './data/aboutdata';
 import Fade from 'react-reveal/Fade';
-// import Fade from 'react-reveal/Fade';
 
+const About = () => {
+    let paragraphs = data.description.map((para, i) => <p key={i} className={styles.para}>{para}</p>);
 
-class About extends Component {
-    render() {
-
-        let paragraphs = data.description.map((para, i) => <p key={i} className={styles.para}>{para}</p>);
-
-        return (
-            <section id="about" className={styles.aboutSection}>
-                <h2>ABOUT</h2>
-                <Fade right>
-                    <div className={styles.flexRow}>
-                        <div className={styles.imgContainer}>    
-                            <Fade delay={1000}>
-                                <img className={styles.headshot} src={data.headshot} alt="Michael Allain headshot"/>
-                            </Fade>
-                            <p className={styles.tagLine}>{data.tagline}</p>
-                        </div>
-                        <div className={styles.flexCol}>
-                            {paragraphs}
-                        </div>
+    return (
+        <section id="about" className={styles.aboutSection}>
+            <h2>ABOUT</h2>
+            <Fade right>
+                <div className={styles.flexRow}>
+                    <div className={styles.imgContainer}>    
+                        <Fade delay={1000}>
+                            <img className={styles.headshot} src={data.headshot} alt="Michael Allain headshot"/>
+                        </Fade>
+                        <p className={styles.tagLine}>{data.tagline}</p>
                     </div>
-                </Fade>
-            </section>
-        );
-    }
+                    <div className={styles.flexCol}>
+                        {paragraphs}
+                    </div>
+                </div>
+            </Fade>
+        </section>
+    );
 }
 
 export default About;
