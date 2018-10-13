bdc8e90ac1098bdd36a5459c07b92ba812dfb4a0
fastlain <michaeldallain@gmail.com>
2018-10-12 17:53:21 -0700

    Convert ProjectsContainer to stateless component

--- src/ProjectsContainer.js
+++ src/ProjectsContainer.js
@@ -1,27 +1,24 @@
-import React, {Component} from 'react';
+import React from 'react';
 import Project from './Project';
 import data from './data/projectdata';
 import Bounce from 'react-reveal/Bounce';
 
-class ProjectsContainer extends Component {
-    render() {
-
-        let projects = data.map((proj, i) => {
-            let bounceside = i%2 === 0;
-            return (
-                <Bounce left={bounceside} right={!bounceside} key={i}>
-                    <Project order={i%2} {...proj}/>
-                </Bounce>
-            );
-        });
-
+const ProjectsContainer = () => {
+    let projects = data.map((proj, i) => {
+        let bounceside = i%2 === 0;
         return (
-            <section id="projects" style={{overflow: 'hidden'}}>
-                <h2>PROJECTS</h2>
-                {projects}
-            </section>
+            <Bounce left={bounceside} right={!bounceside} key={i}>
+                <Project order={i%2} {...proj}/>
+            </Bounce>
         );
-    }
+    });
+
+    return (
+        <section id="projects" style={{overflow: 'hidden'}}>
+            <h2>PROJECTS</h2>
+            {projects}
+        </section>
+    );
 }
 
 export default ProjectsContainer;
