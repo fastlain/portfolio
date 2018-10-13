8e1c1f7f82150d52a1e6b6e49639361bcee0592b
fastlain <michaeldallain@gmail.com>
2018-10-12 17:56:32 -0700

    Convert Projects to stateless component

--- src/Project.js
+++ src/Project.js
@@ -1,44 +1,40 @@
-import React, {Component} from 'react';
+import React from 'react';
 import Technology from './Technology';
 import styles from './Project.module.css';
 
-class Project extends Component {
-    render() {
-        
-        // Project layout will alternate between screenshot on left vs right
-        let flexOrder;
-        if (this.props.order === 0) {
-            flexOrder = styles.flexLeft;
-        } else {
-            flexOrder = styles.flexRight;
-        }
-        
-        return (
-            <div className={styles.project}>
-                <h3 className={styles.title}>{this.props.title}</h3>
-                <div className={styles.flexRow}>
-                    <div className={`${styles.imgContainer} ${flexOrder}`}>
-                        <img className={styles.screenshot} src={this.props.screenshot} alt={this.props.title + " Screenshot"} />            
-                    </div>
-                    <div className={styles.flexCol}>
-                        <p className={styles.para}>
-                            {this.props.description}
-                        </p>
-                        <Technology tech={this.props.technology}/>
-                        <div className={styles.btnCont}>
-                            <a href={this.props.liveLink} target="_blank" rel="noopener noreferrer">
-                                <button className={styles.btn} type="button">Live Demo</button>
-                            </a>
-                            <a href={this.props.gitHubLink} target="_blank" rel="noopener noreferrer">
-                                <button className={styles.btn} type="button">GitHub Repo</button>
-                            </a>
-                            
-                        </div>
+const Project = (props) => { 
+    // Project layout will alternate between screenshot on left vs right
+    let flexOrder;
+    if (props.order === 0) {
+        flexOrder = styles.flexLeft;
+    } else {
+        flexOrder = styles.flexRight;
+    }
+    
+    return (
+        <div className={styles.project}>
+            <h3 className={styles.title}>{props.title}</h3>
+            <div className={styles.flexRow}>
+                <div className={`${styles.imgContainer} ${flexOrder}`}>
+                    <img className={styles.screenshot} src={props.screenshot} alt={props.title + " Screenshot"} />            
+                </div>
+                <div className={styles.flexCol}>
+                    <p className={styles.para}>
+                        {props.description}
+                    </p>
+                    <Technology tech={props.technology}/>
+                    <div className={styles.btnCont}>
+                        <a href={props.liveLink} target="_blank" rel="noopener noreferrer">
+                            <button className={styles.btn} type="button">Live Demo</button>
+                        </a>
+                        <a href={props.gitHubLink} target="_blank" rel="noopener noreferrer">
+                            <button className={styles.btn} type="button">GitHub Repo</button>
+                        </a>
                     </div>
                 </div>
             </div>
-        );
-    }
+        </div>
+    );
 }
 
 export default Project;
