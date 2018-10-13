0a9a6170de441b38e3b810ba99ce32b99f1b1f16
fastlain <michaeldallain@gmail.com>
2018-10-12 18:00:00 -0700

    Convert Banner to stateless component

--- src/Banner.js
+++ src/Banner.js
@@ -1,29 +1,26 @@
-import React, { Component } from 'react';
+import React from 'react';
 import styles from './Banner.module.css';
 import Bounce from 'react-reveal/Bounce';
 import Reveal from 'react-reveal/Reveal';
 
-class Banner extends Component {
-    render() {
-      return (
-        <section className={styles.banner}>
-            <div className={styles.wrapper}>
-              <Bounce left duration={1500}>
-                <h1 className={styles.mainHeading}>Michael Allain</h1>
-              </Bounce>
-              
-              <Reveal effect={styles.growSeparator}>
-                <div className={styles.separator}></div>
-              </Reveal>
-              <Bounce right duration={1500}>
-                
-                <h2 className={styles.subHeading}>FULL STACK WEB DEVELOPER</h2>
-              </Bounce>
-            </div>
-        </section>
-      );
-    }
-  }
-  
-  export default Banner;
-  
+const Banner = () => {
+	return (
+		<section className={styles.banner}>
+			<div className={styles.wrapper}>
+				<Bounce left duration={1500}>
+					<h1 className={styles.mainHeading}>Michael Allain</h1>
+				</Bounce>
+
+				<Reveal effect={styles.growSeparator}>
+					<div className={styles.separator}></div>
+				</Reveal>
+				
+				<Bounce right duration={1500}>
+					<h2 className={styles.subHeading}>FULL STACK WEB DEVELOPER</h2>
+				</Bounce>
+			</div>
+		</section>
+	);
+}
+
+export default Banner;
