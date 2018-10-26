14272d7275eab89899fa878f23adf88ba750e7eb
fastlain <michaeldallain@gmail.com>
2018-10-26 16:22:13 -0700

    Update About description

--- src/data/aboutdata.js
+++ src/data/aboutdata.js
@@ -3,7 +3,7 @@
 const aboutdata = {
     tagline: "Hi! I'm a Web Developer based in Portland, OR.",
     description: [
-        "I'm rapidly learning exciting web technologies through Thinkful’s Full Stack web development program, focusing on the <span>\"MERN\" stack </span> (MongoDB, Express, React, Node). I love bringing great ideas to life by building web apps that are both useful and fun. As a <span> former medical doctor and researcher</span>, I've learned how to master new skills quickly, communicate effectively with people of diverse backgrounds, and manage large amounts of complex information.",
+        "I'm rapidly learning exciting web technologies through Thinkful’s Full Stack web development program, focusing on the <span>\"MERN\" stack </span> (MongoDB, Express, React, Node). I love bringing great ideas to life by building web apps that both make life easier and are enjoyable to use. As a <span> former medical doctor and researcher</span>, I've learned how to master new skills quickly, communicate effectively with people of diverse backgrounds, and manage large amounts of complex information.",
         "When I'm not in front of a keyboard, you'll find me enjoying stereotypical PNW activities like hiking, camping, fishing, gardening, beer tasting, and hunting wild mushrooms.",
         "I am currently <span>seeking a junior developer position</span> in the Portland area. I'm eager both to contribute my existing skills and to learn new tech on the job!"
     ],
