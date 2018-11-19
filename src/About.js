eb25f64aa8e8998527349b189efe26ebc2aa2718
fastlain <michaeldallain@gmail.com>
2018-11-19 08:21:02 -0800

    Make About section Bounce for consistency, prettify

--- src/About.js
+++ src/About.js
@@ -1,19 +1,19 @@
 import React from 'react';
 import styles from './About.module.css';
 import data from './data/aboutdata';
-import Fade from 'react-reveal/Fade';
+import { Fade, Bounce } from 'react-reveal';
 
 const About = () => {
-    let paragraphs = data.description.map((para, i) => <p key={i} className={styles.para} dangerouslySetInnerHTML={{__html: para}}></p>);
+    let paragraphs = data.description.map((para, i) => <p key={i} className={styles.para} dangerouslySetInnerHTML={{ __html: para }}></p>);
 
     return (
         <section id="about" className={styles.aboutSection}>
             <h2>ABOUT</h2>
-            <Fade right>
+            <Bounce right>
                 <div className={styles.flexRow}>
-                    <div className={styles.imgContainer}>    
-                        <Fade delay={1000}>
-                            <img className={styles.headshot} src={data.headshot} alt="Michael Allain headshot"/>
+                    <div className={styles.imgContainer}>
+                        <Fade delay={700}>
+                            <img className={styles.headshot} src={data.headshot} alt="Michael Allain headshot" />
                         </Fade>
                         <p className={styles.tagLine}>{data.tagline}</p>
                     </div>
@@ -21,7 +21,7 @@ const About = () => {
                         {paragraphs}
                     </div>
                 </div>
-            </Fade>
+            </Bounce>
         </section>
     );
 }
