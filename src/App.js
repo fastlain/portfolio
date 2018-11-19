8423704b284d2ca40ae4d492f6234240d2ff28b0
fastlain <michaeldallain@gmail.com>
2018-11-19 08:11:41 -0800

    Add a constructor and prettify

--- src/App.js
+++ src/App.js
@@ -9,47 +9,50 @@ import Particles from 'react-particles-js';
 import particleparams from './particlesConfig/particlesConfig4.json'
 
 class App extends Component {
-	state = {
-		screenWidth: window.innerWidth,
-		screenHeight: window.innerHeight 
-	};
+    constructor() {
+        super();
+        this.state = {
+            screenWidth: window.innerWidth,
+            screenHeight: window.innerHeight
+        };
+    }
 
-	updateScreenDimensions = () => {
-		this.setState({screenWidth: window.innerWidth, screenHeight: window.innerHeight});
-	}
+    updateScreenDimensions = () => {
+        this.setState({ screenWidth: window.innerWidth, screenHeight: window.innerHeight });
+    }
 
-	componentDidMount = () => {
-		this.updateScreenDimensions();
-		window.addEventListener("resize", this.updateScreenDimensions);
-	}
+    componentDidMount = () => {
+        this.updateScreenDimensions();
+        window.addEventListener("resize", this.updateScreenDimensions);
+    }
 
-	componentWillUnmount = () => {
-		window.removeEventListener("resize", this.updateScreenDimensions);
-	}
-	
-	render() {
-		// Dynamically adjust number of particles in banner background based on screen size
-		const screenArea = this.state.screenHeight*this.state.screenWidth;
-		const numParticles = screenArea/8000;
-		particleparams.particles.number.value = Math.floor(numParticles);
-		
-		return (
-			<div>
-				<Header />
-				<Particles width="100%" height="100vh" params={particleparams} style={{position: "absolute"}}/>
-				<Banner />
-				<Element name="scrollAbout">
-					<About />
-				</Element>
-				<Element name="scrollProjects">
-					<ProjectsContainer />
-				</Element>
-				<Element name="scrollContact">
-					<Contact />
-				</Element>
-			</div>
-		);
-	}
+    componentWillUnmount = () => {
+        window.removeEventListener("resize", this.updateScreenDimensions);
+    }
+
+    render() {
+        // Dynamically adjust number of particles in banner background based on screen size
+        const screenArea = this.state.screenHeight * this.state.screenWidth;
+        const numParticles = screenArea / 8000;
+        particleparams.particles.number.value = Math.floor(numParticles);
+
+        return (
+            <div>
+                <Header />
+                <Particles width="100%" height="100vh" params={particleparams} style={{ position: "absolute" }} />
+                <Banner />
+                <Element name="scrollAbout">
+                    <About />
+                </Element>
+                <Element name="scrollProjects">
+                    <ProjectsContainer />
+                </Element>
+                <Element name="scrollContact">
+                    <Contact />
+                </Element>
+            </div>
+        );
+    }
 }
 
 export default App;
