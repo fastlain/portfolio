22787f6fa4224438854fda880e3c0fc708a0cfd5
fastlain <michaeldallain@gmail.com>
2018-11-29 12:08:32 -0800

    Adjust tech column spacing

--- src/Technology.js
+++ src/Technology.js
@@ -1,13 +1,13 @@
 import React from 'react';
-import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
-import {faCode} from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCode } from '@fortawesome/free-solid-svg-icons';
 import styles from './Technology.module.css';
 
-const Technology = ({tech}) => {
+const Technology = ({ tech }) => {
     let useHeadings = tech.length > 1;
     let techdata = tech.map(techGroup => {
         return (
-            <div key={techGroup.category}>
+            <div className={styles.column} key={techGroup.category}>
                 {useHeadings ? <h5 className={styles.catHeading}>{techGroup.category}</h5> : null}
                 <ul className={styles.list}>
                     {techGroup.techs.map(tech => <li className={styles.techItem} key={tech}>{tech}</li>)}
@@ -19,9 +19,9 @@ const Technology = ({tech}) => {
     return (
         <div>
             <h4 className={styles.techHeading}>
-                <FontAwesomeIcon icon={faCode} className={styles.icon}/>
-                    Technology
-                <FontAwesomeIcon icon={faCode} className={styles.icon}/>
+                <FontAwesomeIcon icon={faCode} className={styles.icon} />
+                Technology
+                <FontAwesomeIcon icon={faCode} className={styles.icon} />
             </h4>
             <div className={styles.container}>
                 {techdata}
