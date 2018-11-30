5f1402d4fe67437bfcd3d8426f7f0df6b39d8973
fastlain <michaeldallain@gmail.com>
2018-11-29 16:22:36 -0800

    Add tabindexes to nav links

--- src/Header.js
+++ src/Header.js
@@ -3,21 +3,21 @@ import styles from './Header.module.css';
 import { Link } from 'react-scroll'
 
 const Header = () => {
-	return (
-		<header className={styles.pageHeader}>
-			<nav className={styles.flex}>
-				<Link to="scrollAbout" smooth={true} duration={500} offset={-25}>
-					<span className={styles.navLink}>ABOUT</span>
-				</Link>
-				<Link to="scrollProjects" smooth={true} duration={500} offset={-25}>
-					<span className={styles.navLink}>PROJECTS</span>
-				</Link>
-				<Link to="scrollContact" smooth={true} duration={500}>
-					<span className={styles.navLink}>CONTACT</span>
-				</Link>
-			</nav>
-		</header>
-	);
+    return (
+        <header className={styles.pageHeader}>
+            <nav className={styles.flex}>
+                <Link to="scrollAbout" smooth={true} duration={500} offset={-25} tabindex={1}>
+                    <span className={styles.navLink}>ABOUT</span>
+                </Link>
+                <Link to="scrollProjects" smooth={true} duration={500} offset={-25} tabindex={2}>
+                    <span className={styles.navLink}>PROJECTS</span>
+                </Link>
+                <Link to="scrollContact" smooth={true} duration={500} tabindex={3}>
+                    <span className={styles.navLink}>CONTACT</span>
+                </Link>
+            </nav>
+        </header>
+    );
 }
 
 export default Header;
