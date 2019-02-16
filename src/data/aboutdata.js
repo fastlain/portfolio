1e9383a5b31a6bb3a3bcdb7492db42e6c955f60a
fastlain <michaeldallain@gmail.com>
2019-02-16 14:05:40 -0800

    Remove active job search from about section

--- src/data/aboutdata.js
+++ src/data/aboutdata.js
@@ -4,8 +4,7 @@ const aboutdata = {
     tagline: "Hi! I'm a Web Developer based in Portland, OR.",
     description: [
         "I'm a recent graduate of Thinkful’s Full Stack web development program, focusing on the <span>\"MERN\" stack </span> (MongoDB, Express, React, Node). I love bringing great ideas to life by building web apps that both make life easier and are enjoyable to use. Before changing careers, I worked as a <span>medical doctor and researcher</span>, where I learned to master new skills quickly, communicate effectively with people of diverse backgrounds, and manage large amounts of complex information.",
-        "When I'm not in front of a keyboard, you'll find me enjoying stereotypical PNW activities like hiking, camping, fishing, gardening, beer tasting, and foraging for wild mushrooms.",
-        "I am currently <span>seeking a junior developer position</span> in the Portland area. I'm eager both to contribute my existing skills and to learn new tech on the job!"
+        "When I'm not in front of a keyboard, you'll find me enjoying stereotypical PNW activities like hiking, camping, fishing, gardening, beer tasting, and foraging for wild mushrooms."
     ],
     headshot: "images/headshot.jpg"
 }
