72f77df2a5819d6ec6b6896d583c95c4ac623208
MichaelAllain <michaeldallain@gmail.com>
2021-01-03 13:16:01 -0800

    Update About section content

--- src/data/aboutdata.js
+++ src/data/aboutdata.js
@@ -1,9 +1,10 @@
-// This file contains about information to be imported by React components 
+// This file contains About information to be imported by React components 
 
 const aboutdata = {
     tagline: "Hi! I'm a Software Developer based in Portland, OR.",
     description: [
-        "I'm a recent graduate of Thinkful’s Full Stack web development program, focusing on the <span>\"MERN\" stack </span> (MongoDB, Express, React, Node). I love bringing great ideas to life by building web apps that both make life easier and are enjoyable to use. Before changing careers, I worked as a <span>medical doctor and researcher</span>, where I learned to master new skills quickly, communicate effectively with people of diverse backgrounds, and manage large amounts of complex information.",
+        "I love bringing great ideas to life by building applications that are enjoyable to use and make life and work easier. I graduated from Thinkful’s Full Stack web development program in 2018, focusing on the \"MERN\" stack (<span>MongoDB, Express, React, Node</span>). Since then, I've been working for 6 Degrees Health, an innovative healthcare startup, where I picked up <span>Go, PostgreSQL, TypeScript, and more</span>.", 
+        "Before changing careers, I worked as a medical doctor and researcher, where I learned to master new skills quickly, communicate effectively with people of diverse backgrounds, and manage large amounts of complex information.",
         "When I'm not in front of a keyboard, you'll find me enjoying stereotypical PNW activities like hiking, camping, fishing, gardening, beer tasting, and foraging for wild mushrooms."
     ],
     headshot: "images/headshot.jpg"
